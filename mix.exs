ae50651c836ef77b46a7b987ec108e621b28639f
Dave Lively <davelively14@gmail.com>
2017-09-11 13:50:52 -0400

    Added "web" to elixirc_paths

--- mix.exs
+++ mix.exs
@@ -21,14 +21,14 @@ defmodule Flatfoot.Mixfile do
   end
 
   # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib"]
+  defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
+  defp elixirc_paths(_),     do: ["lib", "web"]
 
   # Specifies your project dependencies.
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.3.0"},
+    [{:phoenix, "~> 1.2.0"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.2"},
      {:postgrex, ">= 0.0.0"},
@@ -39,7 +39,7 @@ defmodule Flatfoot.Mixfile do
      {:exvcr, "~> 0.8", only: :test},
      {:faker, "~> 0.7", only: [:dev, :test]},
      {:ex_machina, "~> 2.0"},
-     {:poison, "~> 3.0"},
+     {:poison, "~> 2.0"},
      {:cors_plug, "~> 1.2"},
      {:secure_random, "~> 0.5"},
      {:httpoison, "~> 0.11.1"},
